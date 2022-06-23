0e0f2858d16cfefdca3944ac758427f5c4cb606b
Ozodbek16 <changeitgo3@gmail.com>
2022-06-23 15:51:44 +0500

    fix: number

--- public/script/ayaks.js
+++ public/script/ayaks.js
@@ -1,22 +1,28 @@
-const inp = document.querySelector('.inp');
-const table = document.querySelector('table');
-let num = 1
+const inp = document.querySelector(".inp");
+const table = document.querySelector("table");
 
-inp.addEventListener('input', (e) => {
-    fetch(`/all/${inp.value.toLowerCase()}`).then(item => item.json()).then((item) => {
-        console.log(item);
-        const html = item.map(people => {
-            return `
+inp.addEventListener("input", (e) => {
+  fetch(`/all/${inp.value.toLowerCase()}`)
+    .then((item) => item.json())
+    .then((item) => {
+          let num = 1;
+      const html = item
+        .map((people) => {
+
+          return `
             <tr>
                 <td class="top">${num++}</td>
                 <td>${people.name} ${people.surname}</td>
                 <td>${people.score}</td>
                 <td>${people.group}</td>
                 <td>${people.month}</td>
-                <td class="btn"><a href="/people/${people._id}" class="edit">Edit</a></td>
+                <td class="btn"><a href="/people/${
+                  people._id
+                }" class="edit">Edit</a></td>
             </tr>
-            `
-        }).join('')
-        table.querySelector('tbody').innerHTML = html
-    })
-})
+            `;
+        })
+        .join("");
+      table.querySelector("tbody").innerHTML = html;
+    });
+});
