78d714c6b5530a300920c9376d1631e86bb0123e
Ozodbek16 <changeitgo3@gmail.com>
2022-06-25 16:19:11 +0500

    feat: design

--- public/script/script.js
+++ public/script/script.js
@@ -4,3 +4,14 @@ const sum  = 1
 for (let i = 0; i < count.length; i++) {
     count[i].innerHTML = i + 1
 }
+
+const navSelect = document.querySelector('.click_btn');
+const hover = document.querySelector('.click_nav ul');
+
+navSelect.addEventListener('click', () => {
+    if(hover.style.display === 'none'){
+        hover.style.display = 'block'
+    }else{
+        hover.style.display = 'none'
+    }
+})
